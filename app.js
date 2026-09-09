2d2e6ac99bea906e88e1b49d75542262f9aa5fd6
Niveditha <niveditha.beesu5@gmail.com>
2026-09-09 13:54:08 +0530

    initial commit - javascript game

---
+++ app.js
@@ -0,0 +1,3 @@
+console.log("js working");
+const form = document.getElementById("myform");
+console.log(form);
---
+++ p.js
@@ -0,0 +1,7 @@
+const student = {
+    fullname : nivedithabeesu,
+    marks : 98.3,
+    printmarks: function(){
+        console.log("marks = ",this.marks);
+    }
+}
